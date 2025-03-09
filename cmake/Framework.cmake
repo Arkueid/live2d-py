8796c1f8f04c95ffecfa07ba4fa45e3885cb2d6b
Arkueid <thetardis@qq.com>
2025-03-09 16:26:16 +0800

    refactor

---
+++ cmake/Framework.cmake
@@ -0,0 +1,16 @@
+set(FRAMEWORK_SOURCE OpenGL)
+# Add Cubism Native Framework.
+add_subdirectory(Framework)
+# Add rendering definition to framework.
+target_compile_definitions(Framework PUBLIC ${CSM_TARGET})
+# Add include path of GLEW to framework.
+target_include_directories(Framework PUBLIC glew/include Main/src)
+# If current compiler is mingw, add some options
+if(CMAKE_SYSTEM_NAME MATCHES "Windows")
+  # extra compile options for mingw on Windows
+  if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+    target_compile_options(Framework PRIVATE -fpermissive -Wconversion-null)
+  endif()  
+endif()
+# Link libraries to framework.
+target_link_libraries(Framework Live2DCubismCore glew_s)
---
+++ cmake/Main.cmake
@@ -0,0 +1,36 @@
+
+add_subdirectory(Main/src)
+
+set_property(TARGET Main PROPERTY CXX_STANDARD 17)
+set_property(TARGET Main PROPERTY CXX_STANDARD_REQUIRED ON)
+
+target_include_directories(Main PUBLIC src)
+
+
+find_package(OpenGL REQUIRED)
+
+if(APPLE)
+  set(CMAKE_CXX_STANDARD 11)
+  set(CMAKE_CXX_STANDARD_REQUIRED ON)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+  
+  set(CMAKE_OSX_ARCHITECTURES "arm64")
+  
+  find_package(OpenGL REQUIRED)
+  find_library(COCOA_LIBRARY Cocoa REQUIRED)
+  find_library(IOKIT_LIBRARY IOKit REQUIRED)
+  find_library(COREVIDEO_LIBRARY CoreVideo REQUIRED)
+endif()
+
+target_link_libraries(Main
+  Framework
+  ${OPENGL_LIBRARIES}
+)
+
+if(APPLE)
+  target_link_libraries(Main
+    ${COCOA_LIBRARY}
+    ${IOKIT_LIBRARY} 
+    ${COREVIDEO_LIBRARY}
+  )
+endif()
