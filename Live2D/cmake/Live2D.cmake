46c81f73a86c38df107d84b4fec16681eef88b88
Arkueid <thetardis@qq.com>
2025-04-29 11:57:30 +0800

    set PIC for linux

--- Live2D/cmake/Live2D.cmake
+++ Live2D/cmake/Live2D.cmake
@@ -2,13 +2,12 @@ get_filename_component(LIVE2D_ROOT ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
 
 message("Live2D Python Root: ${LIVE2D_ROOT}")
 
+include(${CMAKE_CURRENT_LIST_DIR}/Core.cmake)
 if (CMAKE_SYSTEM_NAME MATCHES "Android")
 else()
     include(${CMAKE_CURRENT_LIST_DIR}/Glad.cmake)
     add_library(Live2D::Glad ALIAS glad)
 endif()
-
-include(${CMAKE_CURRENT_LIST_DIR}/Core.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Framework.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Main.cmake)
 
