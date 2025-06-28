0da37f11b8bc1b8e4f83c66a3e1e8a390c288aa8
Cristina Águila <thynetruly@gmail.com>
2025-06-27 22:09:00 -0700

    Add multilingual support (English, Japanese, Chinese) and cross-platform build fixes

--- Live2DViewer/Translations/moe_en.ts
+++ Live2DViewer/Translations/moe_en.ts
@@ -1,6 +1,244 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!--
-Run the update_translations CMake target to populate the source strings in this file.
--->
 <!DOCTYPE TS>
-<TS version="2.1"/>
+<TS version="2.1" language="en_US">
+<context>
+    <name>Live2DView</name>
+    <message>
+        <location filename="../Live2DView.ui" line="14"/>
+        <source>Live2DView</source>
+        <translation>Live2DView</translation>
+    </message>
+    <message>
+        <location filename="../Live2DView.ui" line="51"/>
+        <source>参数</source>
+        <translation>Parameters</translation>
+    </message>
+    <message>
+        <location filename="../Live2DView.ui" line="56"/>
+        <source>部件</source>
+        <translation>Parts</translation>
+    </message>
+    <message>
+        <location filename="../Live2DView.ui" line="61"/>
+        <source>图形网格</source>
+        <translation>Drawables</translation>
+    </message>
+    <message>
+        <location filename="../Live2DView.ui" line="81"/>
+        <source>物理计算</source>
+        <translation>Physics</translation>
+    </message>
+    <message>
+        <location filename="../Live2DView.ui" line="91"/>
+        <source>自动呼吸</source>
+        <translation>Auto Breath</translation>
+    </message>
+    <message>
+        <location filename="../Live2DView.ui" line="101"/>
+        <source>自动眨眼</source>
+        <translation>Auto Blink</translation>
+    </message>
+    <message>
+        <location filename="../Live2DView.ui" line="132"/>
+        <location filename="../Live2DView.ui" line="169"/>
+        <source>Id</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <location filename="../Live2DView.ui" line="137"/>
+        <location filename="../Live2DView.ui" line="174"/>
+        <source>名称</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../Live2DView.ui" line="142"/>
+        <source>控制</source>
+        <translation>Control</translation>
+    </message>
+    <message>
+        <location filename="../Live2DView.ui" line="147"/>
+        <source>值</source>
+        <translation>Value</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../MainWindow.ui" line="14"/>
+        <location filename="../MainWindow.ui" line="118"/>
+        <source>Live2DViewer</source>
+        <translation>Live2DViewer</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="43"/>
+        <source>开始</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="157"/>
+        <source>打开（model3.json)</source>
+        <translation>Open (model3.json)</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="198"/>
+        <source>模型</source>
+        <translation>Model</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="204"/>
+        <location filename="../MainWindow.cpp" line="54"/>
+        <location filename="../MainWindow.cpp" line="80"/>
+        <source>帮助</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="218"/>
+        <source>打开</source>
+        <translation>Open</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="226"/>
+        <source>使用说明</source>
+        <translation>Usage Instructions</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="234"/>
+        <source>关于</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="40"/>
+        <source>打开模型</source>
+        <translation>Open Model</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="40"/>
+        <source>Model Json (*.model3.json)</source>
+        <translation>Model Json (*.model3.json)</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="54"/>
+        <source>
+            &lt;html&gt;
+                &lt;head&gt;
+                    &lt;style&gt;
+                        body {
+                            font-family: &quot;Microsoft YaHei&quot;;
+                        }
+                    &lt;/style&gt;
+                &lt;/head&gt;
+                &lt;body&gt;
+                    &lt;p&gt;
+                        &lt;a href=&quot;https://github.com/Arkueid/live2d-py/&quot;&gt;Live2DViewer&lt;/a&gt; 是一个 Live2D 模型查看器，支持 Cubism 3.0 以上版本的模型。
+                    &lt;/p&gt;
+                    &lt;p&gt;
+                        本程序基于 Qt 6.8.2，使用 C++17 编写。
+                    &lt;/p&gt;
+                    &lt;p&gt;
+                        本程序使用 &lt;a href=&quot;https://github.com/Live2D/&quot;&gt;Live2D Cubism SDK&lt;/a&gt;。
+                    &lt;/p&gt;
+                &lt;/body&gt;
+            &lt;/html&gt;
+        </source>
+        <translation>
+            &lt;html&gt;
+                &lt;head&gt;
+                    &lt;style&gt;
+                        body {
+                            font-family: &quot;Arial&quot;, &quot;Helvetica&quot;, sans-serif;
+                        }
+                    &lt;/style&gt;
+                &lt;/head&gt;
+                &lt;body&gt;
+                    &lt;p&gt;
+                        &lt;a href=&quot;https://github.com/Arkueid/live2d-py/&quot;&gt;Live2DViewer&lt;/a&gt; is a Live2D model viewer that supports Cubism 3.0 and later models.
+                    &lt;/p&gt;
+                    &lt;p&gt;
+                        This program is based on Qt 6.8.2 and written in C++17.
+                    &lt;/p&gt;
+                    &lt;p&gt;
+                        This program uses the &lt;a href=&quot;https://github.com/Live2D/&quot;&gt;Live2D Cubism SDK&lt;/a&gt;.
+                    &lt;/p&gt;
+                &lt;/body&gt;
+            &lt;/html&gt;
+        </translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="80"/>
+        <source>
+        &lt;html&gt;
+            &lt;head&gt;
+                &lt;style&gt;
+                    body {
+                        font-family: &quot;Microsoft YaHei&quot;;
+                    }
+                &lt;/style&gt;
+            &lt;/head&gt;
+            &lt;body&gt;
+                &lt;h1&gt;Live2DViewer 帮助&lt;/h1&gt;
+                &lt;p&gt;
+                    点击画布后：
+                &lt;/p&gt;
+                &lt;ul&gt;
+                    &lt;li&gt;
+                        &lt;p&gt;
+                            通过上下左右键移动模型
+                        &lt;/p&gt;
+                    &lt;/li&gt;
+                    &lt;li&gt;
+                        &lt;p&gt;
+                            通过减号键缩小、等号键放大模型
+                        &lt;/p&gt;
+                    &lt;/li&gt;
+                    &lt;li&gt;
+                        &lt;p&gt;
+                            右键唤出菜单，清空选中状态
+                        &lt;/p&gt;
+                    &lt;/li&gt;
+                &lt;/ul&gt;
+                &lt;p&gt;
+                    部件面板和图形网格面板可点击对应行来选择模型的绘制区域
+                &lt;/p&gt;
+            &lt;/body&gt;
+        &lt;/html&gt;
+    </source>
+        <translation>
+        &lt;html&gt;
+            &lt;head&gt;
+                &lt;style&gt;
+                    body {
+                        font-family: &quot;Arial&quot;, &quot;Helvetica&quot;, sans-serif;
+                    }
+                &lt;/style&gt;
+            &lt;/head&gt;
+            &lt;body&gt;
+                &lt;h1&gt;Live2DViewer Help&lt;/h1&gt;
+                &lt;p&gt;
+                    After clicking on the canvas:
+                &lt;/p&gt;
+                &lt;ul&gt;
+                    &lt;li&gt;
+                        &lt;p&gt;
+                            Use arrow keys to move the model
+                        &lt;/p&gt;
+                    &lt;/li&gt;
+                    &lt;li&gt;
+                        &lt;p&gt;
+                            Use minus key to zoom out, equals key to zoom in
+                        &lt;/p&gt;
+                    &lt;/li&gt;
+                    &lt;li&gt;
+                        &lt;p&gt;
+                            Right click to open context menu and clear selection
+                        &lt;/p&gt;
+                    &lt;/li&gt;
+                &lt;/ul&gt;
+                &lt;p&gt;
+                    Click on rows in the Parts panel and Drawables panel to select model render areas
+                &lt;/p&gt;
+            &lt;/body&gt;
+        &lt;/html&gt;
+    </translation>
+    </message>
+</context>
+</TS>
